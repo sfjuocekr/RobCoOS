689da74f614f414b4d5604e3fdf3813df12ca619
Sjoer van der Ploeg <sfjuocekr@gmail.com>
2016-05-29 13:20:57 +0200

    dunno

--- RobCoOS.h
+++ RobCoOS.h
@@ -1,25 +1,3 @@
 #include <Time.h>
 #include <TimeAlarms.h>
-
-#define WIDTH 128
-#define HEIGHT 36
-
-#define CURSOR_HOME 0
-#define CURSOR_UP 1
-#define CURSOR_DOWN 2
-#define CURSOR_RIGHT 3
-#define CURSOR_LEFT 4
-
-
-#define arrayLength(x) (sizeof(x) / sizeof(*x))
-
-void parseInput(String _input);
-void clearTerminal(boolean _home);
-void moveCursor(uint8_t _direction);
-void moveCursor(uint8_t _steps, int _direction);
-uint8_t consoleHeader();
-uint8_t consoleHeader(String _name);
-void consolePrompt();
-void drawTerminal();
-void drawTerminal(boolean _clear);
-
+#include <RobCoTerm.h>
